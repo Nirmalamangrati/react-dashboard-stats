75919a004c6baa13b0c268cbdabfa98f5abe3936
Nirmalamangrati <157771329+Nirmalamangrati@users.noreply.github.com>
2026-02-02 20:10:19 +0545

    Add new components and update existing files

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
         <div className="font-bold text-lg">
           <img
             src="src/images/Screenshot 2026-02-02 123311.png"
-            className="w-12 h-18 object-contain border-3 border-transparent hover:border-dotted hover:border-purple-500 transition "
+            className="w-4 h-18 object-contain border-3 border-transparent hover:border-dotted hover:border-purple-500 transition "
           />
         </div>
       </div>
